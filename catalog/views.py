fcfacdde057c8aa080c2d5cee3de986f3406702a
Ilya Novikov <illianovikovnitzana@gmail.com>
2024-09-09 23:34:27 +0700

    update catalog/views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,34 +1,54 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.views.generic import DetailView, ListView, TemplateView
+
 
 from catalog.models import Product
 
 
-def contacts(request):
-    if request.method == "POST":
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        print(f'{name} ({email}): {message}')
-
-    return render(request, 'contacts.html')
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/product_list.html'
 
 
-def home(request):
-    return render(request, 'home.html')
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
 
 
-def products(request):
-    products = Product.objects.all()
-    context = {
-        "products": products
-    }
-    return render(request, 'products.html', context)
+class ContactsView(TemplateView):
+    template_name = 'contacts.html'
 
 
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {
-        'product': product
-    }
-    return render(request, 'product_detail.html', context)
+class HomeView(TemplateView):
+    template_name = 'home.html'
 
+# Код, который был заменён лежит внизу.
+
+# def home(request):
+#     return render(request, 'home.html')
+
+
+# def contacts(request):
+#     if request.method == "POST":
+#         name = request.POST.get('name')
+#         email = request.POST.get('email')
+#         message = request.POST.get('message')
+#         print(f'{name} ({email}): {message}')
+#
+#     return render(request, 'contacts.html')
+
+
+# def product_detail(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#     context = {
+#         'product': product
+#     }
+#     return render(request, 'product_detail.html', context)
+
+
+# def products(request):
+#     products = Product.objects.all()
+#     context = {
+#         "products": products
+#     }
+#     return render(request, 'product_list.html', context)

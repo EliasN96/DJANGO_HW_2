744d01276144160b8eb3480b07b8b341610632b9
Ilya Novikov <illianovikovnitzana@gmail.com>
2024-09-10 20:45:30 +0700

    add blog/views.py

---
+++ blog/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+
+from blog.models import Blog
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ('title', 'body', 'image')
+    template_name = 'blog/blog_form.html'
+    success_url = reverse_lazy('blog:blog_list')
+
+
+class BlogListView(ListView):
+    model = Blog
+    template_name = 'blog/blog_list.html'
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+    template_name = 'blog/blog_detail.html'
+    success_url = reverse_lazy('blog:blog_list')
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('title', 'body', 'image')
+    template_name = 'blog/blog_form.html'
+    success_url = reverse_lazy('blog:blog_list')
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    template_name = 'blog/blog_confirm_delete.html'
+    success_url = reverse_lazy('blog:blog_list')

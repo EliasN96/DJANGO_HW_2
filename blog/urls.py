8dc2877cedce70b8bee4589cab64556212c25567
Ilya Novikov <illianovikovnitzana@gmail.com>
2024-09-10 23:40:03 +0700

    upd all urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ app_name = BlogConfig.name
 urlpatterns = [
     path('create/', BlogCreateView.as_view(), name='blog_create'),
     path('', BlogListView.as_view(), name='blog_list'),
-    path('view/<int:pk>/', BlogDetailView.as_view(), name='blog_view'),
-    path('create/<int:pk>/', BlogUpdateView.as_view(), name='blog_edit'),
-    path('delete/<int:pk>/', BlogDeleteView.as_view(), name='blog_delete'),
+    path('view/<int:pk>', BlogDetailView.as_view(), name='blog_view'),
+    path('create/<int:pk>', BlogUpdateView.as_view(), name='blog_edit'),
+    path('delete/<int:pk>', BlogDeleteView.as_view(), name='blog_delete'),
 ]

1672677676b067ca46afad7c249125d40d69871d
Ilya Novikov <illianovikovnitzana@gmail.com>
2024-09-13 00:04:27 +0700

    update blog/models.py

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=100, verbose_name='Заголовок')
-    slug = models.CharField(max_length=255, unique=True, db_index=True, null=True, blank=True, verbose_name="slug")
+    slug = models.CharField(max_length=255, unique=True, db_index=True, null=True, blank=True, verbose_name="url")
     body = models.TextField(max_length=250, verbose_name='Cодержимое', blank=True, null=True)
     image = models.ImageField(upload_to='blog/photo', verbose_name='Изображение(превью)', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания(записи в БД)')

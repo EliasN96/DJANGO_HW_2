2af3646fb49dfad89b3e4f8bedc5d359212a920c
Ilya Novikov <illianovikovnitzana@gmail.com>
2024-09-16 21:10:05 +0700

    update catalog/views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.contrib import messages
+from django.forms import inlineformset_factory
+from django.urls import reverse_lazy, reverse
 from django.views.generic import DetailView, ListView, TemplateView, CreateView, UpdateView, DeleteView
 
-from catalog.forms import ProductForm
-from catalog.models import Product
+from catalog.forms import ProductForm, VersionForm
+from catalog.models import Product, Version
 
 
 class ProductCreateView(CreateView):
@@ -15,7 +16,33 @@ class ProductCreateView(CreateView):
 class ProductUpdateView(UpdateView):
     model = Product
     form_class = ProductForm
-    success_url = reverse_lazy('catalog:product_list')
+
+    def get_success_url(self):
+        return reverse('catalog:product_detail', args=[self.kwargs.get('pk')])
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
+        if self.request.method == "POST":
+            context_data['formset'] = VersionFormset(self.request.POST, instance=self.object)
+        else:
+            context_data['formset'] = VersionFormset(instance=self.object)
+        return context_data
+
+    def form_valid(self, form):
+        formset = self.get_context_data()['formset']
+        self.object = form.save()
+        if form.is_valid() and formset.is_valid():
+            active_versions = [version for version in formset if version.cleaned_data.get('is_active')]
+            if len(active_versions) > 1:
+                messages.error(self.request, 'Выберите только одну активную версию.')
+                return self.render_to_response(self.get_context_data(form=form, formset=formset))
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return super().form_valid(form)
+        else:
+            return self.render_to_response(self.get_context_data(form=form, formset=formset))
 
 
 class ProductListView(ListView):
@@ -41,33 +68,3 @@ class ContactsView(TemplateView):
 class HomeView(TemplateView):
     template_name = 'home.html'
 
-# Код, который был заменён лежит внизу.
-
-# def home(request):
-#     return render(request, 'home.html')
-
-
-# def contacts(request):
-#     if request.method == "POST":
-#         name = request.POST.get('name')
-#         email = request.POST.get('email')
-#         message = request.POST.get('message')
-#         print(f'{name} ({email}): {message}')
-#
-#     return render(request, 'contacts.html')
-
-
-# def product_detail(request, pk):
-#     product = get_object_or_404(Product, pk=pk)
-#     context = {
-#         'product': product
-#     }
-#     return render(request, 'product_detail.html', context)
-
-
-# def products(request):
-#     products = Product.objects.all()
-#     context = {
-#         "products": products
-#     }
-#     return render(request, 'blog_list.html', context)

c5d93664e9bd11b83b9d0be43e05a9a5dbf2f670
Ilya Novikov <illianovikovnitzana@gmail.com>
2024-09-16 21:08:47 +0700

    update catalog/admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from catalog.models import Category, Product
+from catalog.models import Category, Product, Version
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
+    verbose_name = 'Категория'
 
 
 @admin.register(Product)
@@ -12,4 +13,11 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'category')
     list_filter = ('category',)
     search_fields = ('name', 'description',)
+    verbose_name = 'Продукт'
 
+
+@admin.register(Version)
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ('product', 'version_number', 'version_name', 'is_active',)
+    list_filter = ('product',)
+    verbose_name = 'Версия продукта'
